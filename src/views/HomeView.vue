4a954497ccca8ca71699be11ec010eef4d7f04e6
Stefano Consonni <stefanoconsonni@ymail.com>
2023-08-02 15:02:50 +0200

    edit HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="home"><h1>Home</h1></div>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     name: "HomeView",
     components: {},
+    data() {
+      return {
+        projects: [],
+      };
+    },
+    mounted() {
+      fetch("http://localhost:3000/projects").then((res) =>
+        res
+          .json()
+          .then((data) => (this.projects = data))
+          .catch((error) => console.log(error.message))
+      );
+    },
   };
 </script>
