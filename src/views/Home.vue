e2d55e18e0b8c74c5aacbe8f4c439c6284cfef18
Stefano Consonni <stefanoconsonni@ymail.com>
2023-08-04 22:44:58 +0200

    complete Navbar.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
   import SingleProject from "../components/SingleProject.vue";
 
   export default {
-    name: "HomeView",
+    name: "Home",
     components: { SingleProject },
     data() {
       return {
