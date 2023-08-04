43c5cbd42ec71a4aa717ac3b5280f8270e18f08b
Stefano Consonni <stefanoconsonni@ymail.com>
2023-08-04 20:35:13 +0200

    add styles to SingleProject.vue

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -2,6 +2,11 @@
   <div class="project">
     <div class="actions">
       <h3 @click="toggleDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-icons"> edit </span>
+        <span class="material-icons"> delete </span>
+        <span class="material-icons"> done </span>
+      </div>
     </div>
     <div class="details" v-if="showDetails">
       <p>{{ project.details }}</p>
@@ -26,6 +31,10 @@
 </script>
 
 <style scoped>
+  h3 {
+    cursor: pointer;
+  }
+
   .project {
     margin: 20px auto;
     background: white;
@@ -35,7 +44,20 @@
     border-left: 4px solid #e90074;
   }
 
-  h3 {
+  .actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
     cursor: pointer;
+  }
+
+  .material-icons:hover {
+    color: #777;
   }
 </style>
