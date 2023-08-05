29b5a735c91bc60554a3395b14067a07769eef8e
Stefano Consonni <stefanoconsonni@ymail.com>
2023-08-05 14:34:42 +0200

    complete filter-nav feature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="current = $event" :current="current" />
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
@@ -10,13 +11,15 @@
 
 <script>
   import SingleProject from "../components/SingleProject.vue";
+  import FilterNav from "../components/FilterNav.vue";
 
   export default {
     name: "Home",
-    components: { SingleProject },
+    components: { SingleProject, FilterNav },
     data() {
       return {
         projects: [],
+        current: "all",
       };
     },
     mounted() {
@@ -36,6 +39,17 @@
           return project.id === id;
         });
         p.complete = !p.complete;
+      },
+      computed: {
+        filteredProjects() {
+          if (this.current === "completed") {
+            return this.projects.filter((project) => project.complete);
+          }
+          if (this.current === "ongoing") {
+            return this.projects.filter((project) => !project.complete);
+          }
+          return this.projects;
+        },
       },
     },
   };
