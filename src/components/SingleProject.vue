03318a13ba458e6aff7125d8605f3df41957e6b9
Stefano Consonni <stefanoconsonni@ymail.com>
2023-08-05 13:56:35 +0200

    create EditProject.vue

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -3,7 +3,9 @@
     <div class="actions">
       <h3 @click="toggleDetails">{{ project.title }}</h3>
       <div class="icons">
-        <span class="material-icons"> edit </span>
+        <router-link :to="{ name: 'EditProject', params: { id: project.id } }">
+          <span class="material-icons"> edit </span>
+        </router-link>
         <span @click="deleteProject" class="material-icons"> delete </span>
         <span @click="toggleComplete" class="material-icons tick"> done </span>
       </div>
@@ -20,7 +22,7 @@
     data() {
       return {
         showDetails: false,
-        uri: `http://localhost:3000/projects/${this.project.id}`,
+        uri: "http://localhost:3000/projects/" + this.project.id,
       };
     },
     methods: {
